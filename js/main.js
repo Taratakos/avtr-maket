b7b85c127926cc61f200bb47b8302e056efed902
Taras Zarytskyi <taras.zarytskyi@gmail.com>
2023-08-29 17:25:25 +0300

    feat: add  markup and styles to design-slider section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-$(function(){
-
+$(function () {
+  $(".design-slider").slick({
+    dots: false,
+    // щоб показувало 4 картинки
+    slidesToShow: 4, 
+    variableWidth: true,
+    prevArrow:
+      '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="l-arrow">',
+    nextArrow:
+      '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="r-arrow">',
+  });
 });

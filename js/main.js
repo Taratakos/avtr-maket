22eaf39dd0303e46a48d9e209eb06d47bfd9cd34
Taras Zarytskyi <taras.zarytskyi@gmail.com>
2023-08-30 15:24:24 +0300

    feat: add responsive media queries

--- js/main.js
+++ js/main.js
@@ -2,11 +2,20 @@ $(function () {
   $(".design-slider").slick({
     dots: false,
     // щоб показувало 4 картинки
-    slidesToShow: 4, 
+    slidesToShow: 4,
     variableWidth: true,
     prevArrow:
       '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="l-arrow">',
     nextArrow:
       '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="r-arrow">',
+    responsive: [
+      {
+        breakpoint: 361,
+        settings: {
+          variableWidth: false,
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 });
